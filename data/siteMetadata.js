2aafe45b0866d0ec037bebde77088eb923f9c22e
Kevin Kathei Mutunga <limelitechsolutions@gmail.com>
2025-05-17 04:09:55 -0700

    Update logo and favicon

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -12,7 +12,7 @@ const siteMetadata = {
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'kelvinkatheim@gmail.com',
-  github: 'https://github.com',
+  github: 'https://github.com/limelitech',
   x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
